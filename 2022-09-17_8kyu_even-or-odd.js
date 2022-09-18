3c0165d3b616ef5ea0d81d65d9f941e7464b2b24
Cristobal Evensen <cevensen.a@gmail.com>
2022-09-17 21:49:25 -0300

    Coding challenge for 2022-09-17

--- 2022-09-17_8kyu_even-or-odd.js
+++ 2022-09-17_8kyu_even-or-odd.js
@@ -1,5 +1,6 @@
 /*Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.*/
 
+
 // Answer
 
 const evenOrOdd = num => num % 2 == 0 ? 'Even' : 'Odd'
