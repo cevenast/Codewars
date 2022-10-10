b125908c369de74138aeddf4757a99f7bc1b7780
Cristobal Evensen <cevensen.a@gmail.com>
2022-10-10 12:40:27 -0300

    Coding challenge for 2022-10-10

---
+++ 2022-10-10_8kyu_reversed-strings.js
@@ -0,0 +1,17 @@
+/*Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'*/
+
+
+// Answer
+
+
+function solution(str){
+    let reverseStr = ''
+    for (let i = str.length-1; i >= 0; i-- ){
+      reverseStr += str[i]
+    
+    }
+    return reverseStr  
+  }
