1e48ede440349edcc90cf6f5dc41befa6d8ee097
Cristobal Evensen <cevensen.a@gmail.com>
2022-10-27 10:04:16 -0300

    Coding challenge for 2022-10-27

---
+++ 2022-10-27_8kyu_how-good-are-you.js
@@ -0,0 +1,17 @@
+/*There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+
+Note: Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+*/
+
+// Answer
+
+function betterThanAverage(classPoints, yourPoints) {
+    let sum = classPoints.reduce((initialValue, score) => initialValue + score,0) + yourPoints
+    let avg = sum/(classPoints.length + 1)
+    return yourPoints > avg
+  }
